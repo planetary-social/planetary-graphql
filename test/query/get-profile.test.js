163bd3b7cb654cd85313bcd056366edc2da10daf
mixmix <mix@protozoa.nz>
2022-09-22 14:55:57 +1200

    test draft

--- test/query/get-profile.test.js
+++ test/query/get-profile.test.js
@@ -1,10 +1,39 @@
-const { Server } = require('http')
 const test = require('tape')
-const TestBot = require('../test-bot')
+
+const Server = require('../..')
+const Client = require('../client') // apollo
 
 test('get-profile', async t => {
-  const { ssb, apollo } = await TestBot()
+  const server = await Server()
 
-  ssb.close()
+  const client = Client() // TODO
+
+  // publicWebHosting = true
+  const mixDesktopId = '@DIoOBMaI1f0mJg+5tUzZ7vgzCeeHh8+zGta4pOjc+k0=.ed25519'
+
+
+  // TODO : check playground
+
+  const res = await client.query({
+    query: gql`query ($id: String!) {
+      getProfile (id: $id) {
+        name
+        description
+      }
+    }`,
+    variables: { id: mixDesktopId }
+  })
+
+  console.log(JSON.stringify(res.data.getProfile, null, 2))
+
+  t.deepEqual(res.data.getProfile, {
+    name: '',
+    description: ''
+  })
+
+  // has publicWebHosting = undefined
+  const someId = '...' //
+
+  server.close()
   t.end()
 })
