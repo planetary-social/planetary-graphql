fe9ea5fd4cb487b6034d4af6a8b25c03b7e89c23
chereseeriepa <cherese.eriepa@outlook.com>
2022-09-23 11:20:10 +1200

    resolver for getting followers count

--- src/graphql/resolvers/ssb.js
+++ src/graphql/resolvers/ssb.js
@@ -2,6 +2,7 @@ const { promisify: p } = require('util')
 const pull = require('pull-stream')
 const paraMap = require('pull-paraMap')
 const { where, contact, toCallback,  } = require('ssb-db2/operators')
+const get = require('lodash.get')
 
 const SSB = require('../../ssb-server')
 
@@ -16,6 +17,30 @@ module.exports = function Resolvers () {
 
     profile.id = id
     return profile
+  }
+
+  const getFollowersIds = (feedId) => {
+    return new Promise((resolve, reject) => {
+      ssb.db.query(
+        where(
+          contact(feedId)
+        ),
+        toCallback((err, msgs) => {
+          if (err) return reject(err)
+
+          pull(
+            pull.values(msgs),
+            pull.filter(msg => msg.value.content.following),
+            pull.map(msg => msg.value.author),
+            pull.unique(),
+            pull.collect((err, followersIds) => {
+              if (err) reject(err)
+              else resolve(followersIds)
+            })
+          )
+        })
+      )
+    })
   }
 
   return {
@@ -53,36 +78,30 @@ module.exports = function Resolvers () {
           })
         },
         followers: async (parent) => {
-          return new Promise((resolve, reject) => {
-            ssb.db.query(
-              where(
-                  contact(parent.id)
-              ),
-              toCallback((err, msgs) => {
-                  if (err) return reject(err)
+          const ids = await getFollowersIds(parent.id)
 
-                  pull(
-                    pull.values(msgs),
-                    pull.filter(msg => msg.value.content.following),
-                    pull.map(msg => msg.value.author),
-                    pull.unique(),
-                    // TODO: limit?
-                    paraMap((id, cb) => {
-                      getProfile(id)
-                        .then(profile => cb(null, profile))
-                        .catch(err => cb(err))
-                    }, 5),
-                    pull.filter(Boolean),
-                    // TODO: This removes the profiles that came back as null, we might want to show something in place of that
-                    // e.g. someone who hasnt opted in to publicWebHosting
-                    pull.collect((err, followers) => {
-                      if (err) reject(err)
-                      else resolve(followers)
-                    })
-                  )
+          return new Promise((resolve, reject) => {
+            pull(
+              pull.values(ids),
+              paraMap((id, cb) => {
+                getProfile(id)
+                  .then(profile => cb(null, profile))
+                  .catch(err => cb(err))
+              }, 5),
+              pull.filter(Boolean),
+              // TODO: This removes the profiles that came back as null, we might want to show something in place of that
+              // e.g. someone who hasnt opted in to publicWebHosting
+              pull.collect((err, followers) => {
+                if (err) reject(err)
+                else resolve(followers)
               })
             )
           })
+        },
+        followersCount: async (parent) => {
+          const ids = await getFollowersIds(parent.id)
+
+          return get(ids, 'length')
         },
         following: (parent) => {
           return new Promise((resolve, reject) => {
