8b8bed7f1448d94b537cd324e2d1769fac26e8dd
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-04 18:07:56 +1300

    simplify get-profile test, add test-users

--- test/query/get-profile.test.js
+++ test/query/get-profile.test.js
@@ -1,14 +1,37 @@
 const test = require('tape')
 const gql = require('graphql-tag')
-// const { createTestClient } = require('apollo-server-integration-testing')
-const ssbKeys = require('ssb-keys')
 const { promisify: p } = require('util')
-// const GraphqlServer = require('../../src/graphql')
+const parallel = require('run-parallel')
+
 const TestBot = require('../test-bot')
+const { alice, bob, carol } = require('../lib/test-users')
+
+// TODO: move to lib or test-bot
+function setupUsers (ssb, cb) {
+  parallel(
+    [alice, bob, carol].map(user => (_cb) => {
+      ssb.db.create({
+        content: {
+          type: 'about',
+          about: user.id,
+          name: user.name,
+          publicWebHosting: user.publicWebHosting
+        },
+        keys: user.keys
+      }, _cb)
+    }),
+    (err) => {
+      if (err) cb(err)
+      else cb(null, null)
+    }
+  )
+}
 
 test('get-profile', async t => {
   t.plan(6)
   const { apollo, ssb } = await TestBot()
+
+  await p(setupUsers)(ssb)
 
   // helpers
 
@@ -29,53 +52,17 @@ test('get-profile', async t => {
     )
   }
 
-  const createProfile = (profile) => {
-    return p(ssb.db.create)(profile)
-  }
-
-  const alice = ssbKeys.generate()
-  const bob = ssbKeys.generate()
-  const carol = ssbKeys.generate()
-
-  // create alices profile
-  await createProfile({
-    content: {
-      type: 'about',
-      about: alice.id,
-      name: 'alice'
-      // publicWebHosting: undefined
-    },
-    keys: alice
-  })
-
+  // get a user who has publicWebHosting=undefined
   let res = await getProfile(alice.id)
   t.error(res.errors, 'get alices profile returns no errors')
   t.false(res.data.getProfile, 'returns no profile for alice')
 
-  await createProfile({
-    content: {
-      type: 'about',
-      about: bob.id,
-      name: 'bob',
-      publicWebHosting: false
-    },
-    keys: bob
-  })
-
+  // get a user who has publicWebHosting=false
   res = await getProfile(bob.id)
   t.error(res.errors, 'get bobs profile returns no errors')
   t.false(res.data.getProfile, 'returns no profile for bob')
 
-  await createProfile({
-    content: {
-      type: 'about',
-      about: carol.id,
-      name: 'carol',
-      publicWebHosting: true
-    },
-    keys: carol
-  })
-
+  // get a user who has publicWebHosting=true
   res = await getProfile(carol.id)
   t.error(res.errors, 'get carols profile returns no errors')
   t.deepEqual(
