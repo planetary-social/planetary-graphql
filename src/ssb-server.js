05a6669f7575eea3d8f0303dc3b734c93ec1435d
mixmix <mix@protozoa.nz>
2022-10-11 20:44:00 +1300

    configure to run without scheduler, fix pub addresses

--- src/ssb-server.js
+++ src/ssb-server.js
@@ -11,7 +11,6 @@ const DB_PATH = join(__dirname, '../db')
 module.exports = function SSB (opts = {}) {
   const stack = SecretStack({ caps })
     .use([
-
       require('ssb-db2/core'),
       require('ssb-db2/compat/publish'),
       require('ssb-classic'),
@@ -20,7 +19,6 @@ module.exports = function SSB (opts = {}) {
       require('ssb-db2/compat/ebt'),
       // require('ssb-db2/compat/post'),
       require('ssb-db2/compat/db')
-
       // require('ssb-db2/migrate'),
     ])
     .use(require('ssb-about-self'))
@@ -28,8 +26,20 @@ module.exports = function SSB (opts = {}) {
 
     .use(require('ssb-friends'))
     .use(require('ssb-lan'))
-    .use(require('ssb-conn'))
     .use(require('ssb-ebt'))
+    // .use(require('ssb-conn'))
+    .use(require('ssb-conn/core'))
+    .use(require('ssb-conn/compat')) // for ssb.gossip
+    // use a minimal ssb-conn, exlcuding the scheduler,
+    // which would otherwise prune connections down to 3
+
+  // Staltz recommends writing own scheduler, see README for template
+  // simple idea could be:
+  // 1. bootstrap with hosts above
+  // 2. live query to discover 'pub' messages with hosts
+  // 3. cycle through a series of different connections to discover more messages
+  // outstanding question: getStatus progress stuck?
+
     .use(require('ssb-replication-scheduler'))
     .use(require('ssb-blobs'))
     .use(require('ssb-serve-blobs'))
@@ -62,7 +72,7 @@ module.exports = function SSB (opts = {}) {
         [
           (cb) => ssb.friends.isFollowing({ source: ssb.id, dest: id }, cb),
           (isFollowing, cb) => {
-            if (isFollowing) cb(null, null)
+            if (isFollowing) cb(null, true)
             else ssb.friends.follow(id, { state: true }, cb)
           },
           (data, cb) => {
--- src/ssb-sync.js
+++ src/ssb-sync.js
@@ -1,10 +1,15 @@
 const pull = require('pull-stream')
 const SSB = require('./ssb-server')
 
-const ssb = SSB()
+const ssb = SSB({
+  ebt: {
+    // logging: true
+  }
+})
 
 pull(
   ssb.conn.peers(),
+  pull.map(updates => updates.map(update => [update[0], update[1].state])),
   pull.log()
 )
 
@@ -14,3 +19,9 @@ pull(
   pull.asyncMap((_, cb) => setTimeout(cb, 5000)),
   pull.drain()
 )
+
+ssb.db.onMsgAdded(ev => {
+  if (ev.kvt.value.author === ssb.id) {
+    console.log(JSON.stringify(ev.kvt.value.content, null, 2))
+  }
+})
