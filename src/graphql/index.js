d3e53077aa8e7901df849ae998751f435a910e61
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-12 19:02:03 +1300

    remove cors config

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -19,44 +19,7 @@ module.exports = function GraphqlServer (ssb) {
 
     const app = express()
 
-    app.use(cors({
-      origin (origin, cb) {
-        switch (process.env.NODE_ENV) {
-          case undefined: // if NODE_ENV not set, apply production standards
-          case 'production':
-          case 'staging':
-            // if (origin === undefined) return cb(null, true)
-
-            // Graphql
-            if (origin === `http://127.0.0.1:${opts.port}`) return cb(null, true)
-            if (origin === `http://localhost:${opts.port}`) return cb(null, true)
-            if (origin === `http://157.230.72.191:${opts.port}`) return cb(null, true)
-
-            // Vite
-            if (origin === 'https://planetary-social.github.io/rooms-frontend') return cb(null, true)
-
-            return cb(new Error('CORS issue')) // give no hints
-
-          case 'development':
-          case 'test':
-            // if (origin === undefined) return cb(null, true)
-
-            // Graphql
-            if (origin === `http://localhost:${opts.port}`) return cb(null, true)
-            if (origin === `http://127.0.0.1:${opts.port}`) return cb(null, true)
-
-            // Vite
-            if (origin === 'http://localhost:5173') return cb(null, true)
-            if (origin === 'http://127.0.0.1:5173') return cb(null, true)
-
-            return cb(new Error(`CORS issue - unexpected header 'Origin': '${origin}'`))
-
-          default:
-            cb(new Error(`invalid NODE_ENV - ${process.env.NODE_ENV}`))
-        }
-      },
-      credentials: true // NOTE: required by the frond end when credentials=include for remote origin
-    }))
+    app.use(cors())
 
     const httpServer = http.createServer(app)
     const resolvers = SSBResolvers(ssb)
