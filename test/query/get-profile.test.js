88d64c5699a78f8e467ecbda5215c68df2cd4224
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-13 16:10:04 +1300

    update URI for blobs to point to graphql.planetary.social

--- test/query/get-profile.test.js
+++ test/query/get-profile.test.js
@@ -12,7 +12,7 @@ test('getProfile', async t => {
   // init users
   const alice = await createUser('alice') // publicWebHosting=undefined
   const bob = await createUser('bob', { publicWebHosting: false }) // publicWebHosting=false
-  const carol = await createUser('carol', { publicWebHosting: true }) // publicWebHosting=true
+  const carol = await createUser('carol', { publicWebHosting: true, image: '&Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD/Uu/nDp+KnMg=.sha256' }) // publicWebHosting=true
 
   // get a user who has publicWebHosting=undefined
   let profile = await getProfile(alice.id)
@@ -29,13 +29,43 @@ test('getProfile', async t => {
     {
       id: carol.id,
       name: 'carol',
-      image: null,
+      image: 'http://localhost:26835/get/%26Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD%2FUu%2FnDp%2BKnMg%3D.sha256',
       following: [],
       followingCount: 0,
       followers: [],
       followersCount: 0
     },
     'returns profile for carol who has publicWebHosting enabled'
+  )
+
+  ssb.close()
+})
+
+test('getProfile (staging)', async t => {
+  t.plan(2)
+  process.env.NODE_ENV = 'staging'
+
+  const { apollo, ssb } = await TestBot()
+  const createUser = CreateUser(ssb)
+  const getProfile = GetProfile(apollo, t)
+
+  // init users
+  const carol = await createUser('carol', { publicWebHosting: true, image: '&Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD/Uu/nDp+KnMg=.sha256' }) // publicWebHosting=true
+
+  // get a user who has publicWebHosting=true
+  const profile = await getProfile(carol.id)
+  t.deepEqual(
+    profile,
+    {
+      id: carol.id,
+      name: 'carol',
+      image: `${process.env.BLOBS_URL}/%26Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD%2FUu%2FnDp%2BKnMg%3D.sha256`,
+      following: [],
+      followingCount: 0,
+      followers: [],
+      followersCount: 0
+    },
+    'returns correct image URI'
   )
 
   ssb.close()
