596ca56d4cd0d31d29c12f537c5e9fe5c28b62e9
mix irving <mix@protozoa.nz>
2022-12-05 17:32:18 +1300

    wip

--- src/ssb-room-plugin.js
+++ src/ssb-room-plugin.js
@@ -1,7 +1,26 @@
 /**
- * The sole purpose of this plugin is to declare the remote manifest,
- * nothing else.
+ * This plugin serves two purposes:
+ * 1. declare the remote manifest (required to make RPC calls!)
+ * 2. provide local getters for known room state
  */
+
+const fetch = require('node-fetch')
+const pull = require('pull-stream')
+const pullFlatMap = require('pull-flatmap')
+const pullParaMap = require('pull-paramap')
+const ref = require('ssb-ref')
+
+const MINUTE = 60 * 1000
+
+const dummyRemoteApi = {
+  // attendants: () => pull.error(new Error('not implemented on the client')),
+  metadata (cb) { cb(new Error('not implemented on the client')) },
+  members: () => pull.error(new Error('not implemented on the client')),
+  listAliases () { throw new Error('not implemented on the client') }
+  // registerAlias (_alias, _sig, cb) { },
+  // revokeAlias (_alias, cb) { }
+}
+
 module.exports = {
   name: 'room',
   version: '1.0.0',
@@ -13,28 +32,132 @@ module.exports = {
     // registerAlias: 'async',
     // revokeAlias: 'async',
   },
-  init () {
+  init (ssb) {
+    if (!process.env.ROOM_HOST) throw new Error('missing env: ROOM_HOST')
+    if (!process.env.ROOM_KEY) throw new Error('missing env: ROOM_KEY')
+
+    const state = {
+      address: buildMultiserverAddress(),
+      name: null,
+      notices: null,
+      members: new Map()
+    }
+
+    const runUpdate = () => updateRoomData(ssb, state)
+
+    runUpdate()
+    setInterval(runUpdate, 5 * MINUTE)
+
     return {
-      // attendants () {
-      //   return pull.error(new Error('not implemented on the client'))
-      // },
-
-      metadata (cb) {
-        cb(new Error('not implemented on the client'))
+      ...dummyRemoteApi,
+      get id () {
+        return process.env.ROOM_KEY
       },
-      members () {
-        throw new Error('not implemented on the client')
+      get address () {
+        return state.address
       },
-      listAliases () {
-        throw new Error('not implemented on the client')
+      get name () {
+        return state.name
+      },
+      get notices () {
+        return state.notices
+      },
+      get members () {
+        return Array.from(state.members.keys())
+      },
+      getMember (id) {
+        return state.members.get(id)
       }
-
-      // registerAlias (_alias, _sig, cb) {
-      //   cb(new Error('not implemented on the client'))
-      // },
-      // revokeAlias (_alias, cb) {
-      //   cb(new Error('not implemented on the client'))
-      // }
+      // getMemberByAlias
     }
   }
+}
+
+function updateRoomData (ssb, state) {
+  /* Room Notices */
+  fetch('https://' + process.env.ROOM_HOST + '/notice/list' + '?encoding=json')
+    .then(res => res.json())
+    .then(notices => {
+      if (notices.error) return
+
+      state.notices = notices
+    })
+    .catch(err => console.log('getRoomNotices error:', err)) // returns undefined
+
+  /* Room Name + Members */
+  ssb.conn.connect(state.address, (err, rpc) => {
+    // NOTE - here we don't currently disconnect from the room after this
+    // TODO - test what happens when multiple connections
+    if (err) return console.error('failed to connect to room', err)
+
+    let processing = 2
+    const done = () => {
+      processing--
+      if (processing > 0) return
+      rpc.close((err) => { // eslint-disable-line
+        // if (err) console.error(err)
+        console.log('disconnected from room')
+      })
+    }
+
+    rpc.room.metadata((err, data) => {
+      if (err) return console.error(err)
+      state.name = data.name
+      done()
+    })
+
+    pull(
+      rpc.room.members({}),
+      pullFlatMap(arr => arr),
+
+      /* Follow members (privately) */
+      pullParaMap((member, cb) => {
+        ssb.friends.isFollowing({ source: ssb.id, dest: member.id }, (err, isFollowing) => {
+          if (err) {
+            console.error(err)
+            cb(null, member)
+          }
+
+          if (!isFollowing) {
+            ssb.friends.follow(member.id, { state: true, recps: [ssb.id] }, () => {})
+          }
+          cb(null, member)
+        })
+      }, 5),
+
+      /* Load current aliases */
+      pullParaMap((member, cb) => {
+        rpc.room.listAliases(member.id, (err, aliases) => {
+          if (err) console.error(err)
+          else member.aliases = aliases
+
+          cb(null, member)
+        })
+      }, 5),
+
+      pull.drain(
+        member => {
+          state.members.set(member.id, member)
+        },
+        err => {
+          if (err) console.error('rpc.room.members error', err)
+          done()
+        }
+      )
+    )
+  })
+}
+
+function buildMultiserverAddress () {
+  const objAddr = {
+    host: process.env.ROOM_HOST,
+    port: process.env.ROOM_PORT ? Number(process.env.ROOM_PORT) : 8008,
+    key: process.env.ROOM_KEY
+  }
+
+  if (!ref.isAddress(objAddr)) {
+    throw new Error('ROOM_HOST or ROOM_KEY was invalid and couldnt parse the multiserver address')
+  }
+
+  return ref.toMultiServerAddress(objAddr)
 }
--- src/ssb-server.js
+++ src/ssb-server.js
@@ -2,12 +2,10 @@ const SecretStack = require('secret-stack')
 const caps = require('ssb-caps')
 const ssbKeys = require('ssb-keys')
 const { join } = require('path')
-const waterfall = require('run-waterfall')
 const pull = require('pull-stream')
 const flatMap = require('pull-flatmap')
 
-const peers = require('./peers')
-const DB_PATH = join(__dirname, '../db')
+const DB_PATH = process.env.DB_PATH || join(__dirname, '../db')
 
 function startServer (opts = {}) {
   const stack = SecretStack({ caps })
@@ -15,7 +13,7 @@ function startServer (opts = {}) {
       require('ssb-db2/core'),
       require('ssb-db2/compat/publish'),
       require('ssb-classic'),
-      // require('ssb-box'),
+      require('ssb-box'),
       // require('ssb-box2'),
       require('ssb-db2/compat/ebt'),
       // require('ssb-db2/compat/post'),
@@ -23,51 +21,33 @@ function startServer (opts = {}) {
 
       // require('ssb-db2/migrate'),
     ])
-    .use(require('ssb-about-self'))
-    .use(require('ssb-threads'))
 
     .use(require('ssb-friends'))
+
     .use(require('ssb-lan'))
     .use(require('ssb-conn'))
     .use(require('ssb-ebt'))
     .use(require('ssb-replication-scheduler'))
+    .use(require('ssb-room-client'))
+
+    .use(require('ssb-about-self'))
+    .use(require('ssb-threads'))
+
     .use(require('ssb-blobs'))
     .use(require('ssb-serve-blobs'))
+
     .use(require('./ssb-room-plugin'))
 
   return stack({
     keys: ssbKeys.loadOrCreateSync(join(DB_PATH, 'secret')),
     path: DB_PATH,
-    friends: { hops: 6 },
+    friends: { hops: 2 },
     // lan: { legacy: false },
-    ...opts
-  })
-}
 
-function seedReplication (ssb) {
-  peers.forEach(({ name, id, host, invite }) => {
-    if (id) {
-      waterfall(
-        [
-          (cb) => ssb.friends.isFollowing({ source: ssb.id, dest: id }, cb),
-          (isFollowing, cb) => {
-            if (isFollowing) cb(null, null)
-            else ssb.friends.follow(id, { state: true }, cb)
-          },
-          (data, cb) => {
-            if (invite) ssb.invite.use(invite, cb)
-            else cb(null, null)
-          },
-          (data, cb) => {
-            if (host) ssb.conn.connect(host, cb)
-            else cb(null)
-          }
-        ],
-        (err, connection) => {
-          if (err) console.error(err)
-        }
-      )
-    }
+    // TODO - configure connections
+    // - ssb-rooms-client: https://github.com/ssbc/ssb-room-client
+    // - ssb-config: https://github.com/ssbc/ssb-config/
+    ...opts
   })
 }
 
@@ -102,12 +82,20 @@ function logging (ssb) {
     pull.through(() => console.log(' ')),
     flatMap(evs => evs),
     pull.asyncMap((ev, cb) => {
-      const feedId = ev[1].key
+      const { key: feedId, state } = ev[1]
+
+      const output = {
+        state,
+        name: feedId
+      }
+      if (output.name === ssb.room.id) {
+        output.name += ' (my room)'
+        return cb(null, output)
+      }
+
       ssb.aboutSelf.get(feedId, (err, details) => {
-        cb(null, {
-          state: ev[1].state,
-          name: err ? feedId : (details.name || feedId)
-        })
+        if (!err && details.name) output.name = details.name
+        cb(null, output)
       })
     }),
     pull.drain(({ state, name }) => {
@@ -137,7 +125,6 @@ module.exports = function SSB (opts = {}) {
   if (process.env.LOGGING) logging(ssb)
 
   ssb.lan.start()
-  seedReplication(ssb)
 
   requestAvatars(ssb)
 
